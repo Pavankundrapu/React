aec5585ed52ad2736568b8c488f93830156be177
Pavan Kumar Kundrapu <iampavanhere@gmail.com>
2024-06-02 08:08:47 +0530

    first project

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,12 @@
 import Chai from "./chai.jsx"
 function App() {
+
+  const username = 'chai aur code';
+
   return (
     <>
     <Chai/>
-    <p>welcome to react flow</p>
+    <p>welcome to react flow {username}</p>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function Myapp()
+{
+  return (
+    <div>
+    <h2>this is a fun task that is done by me</h2>
+  </div>
+  )
+}
+/* const reactElement = {
+  type : 'a',
+  props : {
+    href : 'https://google.com',
+    target : '_blank'
+  },
+  children : 'click me to visit google'
+}*/
+
+const reactElement = React.createElement(
+  'a',
+  {href : 'https://google.com', target : '_blank'},
+  'click me to visit')
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit our google</a>
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <App/>
+  )
