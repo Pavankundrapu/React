f8b0d6f31a46a6b41150a6b27f4150f5e527a336
Pavan Kumar Kundrapu <iampavanhere@gmail.com>
2024-07-11 16:00:29 +0530

    store setup and loading info

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,15 +1,45 @@
 import './App.css'
-import conf from './conf/conf'
+import React, { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth';
+import {login,logout} from './store/authSlice'
+import {Header,Footer} from './components'
+import { Outlet } from 'react-router-dom';
 
 function App() {
 
-   console.log(conf.appwriteBucketId)
-   
-  return (
-    <>
-     <h1>this is our app in appwrite</h1>
-    </>
-  )
+   const [loading, setLoading] = useState(true);
+   const dispatch = useDispatch();
+  
+   useEffect(() => {
+
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData)
+      { 
+        dispatch(login({userData}));
+      }
+      else
+      {
+        dispatch(logout());
+      }
+    })
+    .finally(() => setLoading(false));
+   }, []);
+
+
+//this is conditional redeering of the component.
+ return !loading ? (
+  <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+    <div className='w-full block'>
+      <Header/>
+      <main>
+       todo: <Outlet/>
+      </main>
+      <Footer/>
+    </div>
+  </div>
+ ) : <div>Loading...</div>;
 }
 
 export default App
